0e8076d7eca2b8fc9ec83e9e3a762ab279c9f94b
Luc Bourdil <boulu72@ville-noumea.nc>
2013-04-23 00:46:28 +0000

    Test de remplacer et valider 2

--- src/main/java/nc/mairie/siale/viewmodel/AccueilModel.java
+++ src/main/java/nc/mairie/siale/viewmodel/AccueilModel.java
@@ -29,6 +29,7 @@ public class AccueilModel extends SelectorComposer<Component>{
 	/**
 	 * test de com3
 	 * test de com4
+	 * test de com5
 	 */
 	private static final long serialVersionUID = 446527977528558574L;
 	
