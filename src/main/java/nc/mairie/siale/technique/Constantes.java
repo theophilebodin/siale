dd421300167ebb063b396daa46841b4228cda154
Luc Bourdil <boulu72@ville-noumea.nc>
2013-05-06 02:29:53 +0000

    sequences par entity

--- src/main/java/nc/mairie/siale/technique/Constantes.java
+++ src/main/java/nc/mairie/siale/technique/Constantes.java
@@ -4,9 +4,9 @@ import nc.mairie.siale.domain.Droit;
 
 public class Constantes {
 
-	transient static public Droit droitControleur=Droit.findDroit(new Long(0));
+	transient static public Droit droitControleur=Droit.findDroit(new Long(1));
 	
-	transient static public Droit droitAdmin=Droit.findDroit(new Long(1));
+	transient static public Droit droitAdmin=Droit.findDroit(new Long(2));
 
 
 }
